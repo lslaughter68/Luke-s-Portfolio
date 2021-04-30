8943603ef4f00a7963f8dc5edb1b7e53e540c1a7
lslaughter68 <82115725+lslaughter68@users.noreply.github.com>
2021-04-30 18:16:46 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var song;
+var slider;
+var sliderRate;
+var sliderPan;
+
+function setup() {
+  createCanvas(200,200);
+  song = loadSound("Twisted Tea_11_11_20.mp3", loaded);
+  slider = createSlider(0, 1, 0.5, 0.01);
+  sliderRate = createSlider(0, 2, 1, 0.01);
+  sliderPan = createSlider(-1, 1, 0, 0.01);
+}
+
+function loaded() {
+  song.play();
+}
+
+function draw() {
+  background(255);
+  textSize(18);
+  text('Volume left', 10, 30);
+  text('Rate center', 10, 80);
+  text('Pan right', 10, 130);
+  song.setVolume(slider.value());
+  song.rate(sliderRate.value());
+  song.pan(sliderPan.value());
+}
