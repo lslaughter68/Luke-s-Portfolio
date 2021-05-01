74ba4c81fc07b7b0ebef9376494b988e6cd2505b
lslaughter68 <82115725+lslaughter68@users.noreply.github.com>
2021-04-30 19:19:06 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,10 +2,13 @@ var song;
 var slider;
 var sliderRate;
 var sliderPan;
+var button;
 
 function setup() {
   createCanvas(200,200);
   song = loadSound("Twisted Tea_11_11_20.mp3", loaded);
+  button = createButton("play");
+  button.mousePressed(togglePlaying);
   slider = createSlider(0, 1, 0.5, 0.01);
   sliderRate = createSlider(0, 2, 1, 0.01);
   sliderPan = createSlider(-1, 1, 0, 0.01);
@@ -13,6 +16,15 @@ function setup() {
 
 function loaded() {
   song.play();
+}
+
+function togglePlaying() {
+  if (!song.isPlaying()) {
+  song.play();
+  button.html("pause");
+} else {
+  song.pause();
+  button.html("play");
 }
 
 function draw() {
